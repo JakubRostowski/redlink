ef03510560280df97d336901c6b251a5379054b6
JakubRostowski <rostowskijakub@gmail.com>
2022-08-04 13:12:04 +0200

    error catching precise

--- src/main/java/com/example/demo/SendSmsService.java
+++ src/main/java/com/example/demo/SendSmsService.java
@@ -31,7 +31,7 @@ public class SendSmsService {
     private SendSmsMessageResponse mapToSendSmsMessageResponse(HttpResponse<String> response)
     {
         SendSmsMessageResponse sendSmsMessageResponse = gson.fromJson(response.getBody(), SendSmsMessageResponse.class);
-        if (sendSmsMessageResponse.getData() == null) {
+        if (sendSmsMessageResponse.getMeta().getStatus() == 200) {
             throw new RuntimeException("An error has occurred: " + sendSmsMessageResponse.getMeta().toString());
         }
         return sendSmsMessageResponse;
